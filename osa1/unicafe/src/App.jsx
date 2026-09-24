ee8d5db98b0dd644fed09a6aac08cd0f1f551f03
brynde <brynde@gmail.com>
2026-09-24 15:41:17 +0300

    Add task 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,7 +8,10 @@ const Button = (props) => (
 
 const StatisticsLine = ({text, value}) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -28,12 +31,16 @@ const Statistics = (props) => {
 
   return (
     <div>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={all} />
-      <StatisticsLine text="average" value={average} />
-      <StatisticsLine text="positive" value={positive + '%'} />
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={positive + '%'} />
+        </tbody>
+      </table>
     </div>
   )
 }
